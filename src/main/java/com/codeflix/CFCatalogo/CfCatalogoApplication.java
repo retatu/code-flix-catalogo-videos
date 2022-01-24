dddf88fc5ceb5fef25daa5cf878a0dbb124fc86a
Matheus <mrehbein45@gmail.com>
2022-01-24 16:43:34 -0300

    estrutura

---
+++ src/main/java/com/codeflix/CFCatalogo/CfCatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.codeflix.CFCatalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfCatalogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeflix/CFCatalogo/CfCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeflix.CFCatalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
