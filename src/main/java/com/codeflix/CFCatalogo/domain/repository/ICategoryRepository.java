2f45f5eaf356b100cd36190a78dad23ab91d621c
Matheus <mrehbein45@gmail.com>
2022-01-28 16:53:56 -0300

    repository structure

---
+++ src/main/java/com/codeflix/CFCatalogo/domain/repository/ICategoryRepository.java
@@ -0,0 +1,7 @@
+package com.codeflix.CFCatalogo.domain.repository;
+
+import com.codeflix.CFCatalogo.domain.entity.Category;
+
+public interface ICategoryRepository {
+	Iterable<Category> findAllCategories();
+}
---
+++ src/main/java/com/codeflix/CFCatalogo/infrastructure/mysql/MySQLCategoryImplementation.java
@@ -0,0 +1,14 @@
+package com.codeflix.CFCatalogo.infrastructure.mysql;
+
+import com.codeflix.CFCatalogo.domain.entity.Category;
+import com.codeflix.CFCatalogo.domain.repository.ICategoryRepository;
+
+public class MySQLCategoryImplementation implements ICategoryRepository {
+
+	@Override
+	public Iterable<Category> findAllCategories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
