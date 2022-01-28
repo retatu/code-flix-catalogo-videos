34437236514507f92ec0cb58a6e8d0e8e70a2e21
Matheus <mrehbein45@gmail.com>
2022-01-28 17:02:07 -0300

    spring data implementation

--- src/main/java/com/codeflix/CFCatalogo/infrastructure/mysql/MySQLCategoryImplementation.java
+++ src/main/java/com/codeflix/CFCatalogo/infrastructure/mysql/MySQLCategoryImplementation.java
@@ -3,12 +3,20 @@ package com.codeflix.CFCatalogo.infrastructure.mysql;
 import com.codeflix.CFCatalogo.domain.entity.Category;
 import com.codeflix.CFCatalogo.domain.repository.ICategoryRepository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MySQLCategoryImplementation implements ICategoryRepository {
+
+	private SpringDataCategoryRepository springDataCategoryRepository;
+
+	public MySQLCategoryImplementation(SpringDataCategoryRepository springDataCategoryRepository){
+		this.springDataCategoryRepository = springDataCategoryRepository;
+	}
 
 	@Override
 	public Iterable<Category> findAllCategories() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.springDataCategoryRepository.findAll();
 	}
 
 }
---
+++ src/main/java/com/codeflix/CFCatalogo/infrastructure/mysql/SpringDataCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.codeflix.CFCatalogo.infrastructure.mysql;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface SpringDataCategoryRepository extends PagingAndSortingRepository {
+
+}
